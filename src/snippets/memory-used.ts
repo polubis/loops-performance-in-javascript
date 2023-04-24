4d5a98a08403123513b4a7f793be0abb06f92576
polubis <polubis@wp.pl>
2023-04-24 08:22:51 +0000

    Update comments

--- src/snippets/memory-used.ts
+++ src/snippets/memory-used.ts
@@ -6,7 +6,7 @@ const sum = (...items: number[]): number => {
 };
 
 // We are converting to a number and rounding to four decimal places.
-// Different results are displayed with each use but are very close to each other.
+// Different results are displayed with each use but they're very close to each other.
 console.log(Number(measure(() => sum(1, 2, 3, 4, 5, 6)).toFixed(4))); // 0.0162
 console.log(Number(measure(() => sum(1, 2, 3, 4, 5, 6)).toFixed(4))); // 0.0151
 console.log(Number(measure(() => sum(1, 2, 3, 4, 5, 6)).toFixed(4))); // 0.0161
